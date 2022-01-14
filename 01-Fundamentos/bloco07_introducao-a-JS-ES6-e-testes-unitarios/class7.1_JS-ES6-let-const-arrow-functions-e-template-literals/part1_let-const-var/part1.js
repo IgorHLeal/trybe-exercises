a6b969a7795921c7d7958d7278698f3bd33c1543
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-14 09:18:35 -0300

    Exercícios da Parte 1 concluídos.

--- 01-Fundamentos/bloco07_introducao-a-JS-ES6-e-testes-unitarios/class7.1_JS-ES6-let-const-arrow-functions-e-template-literals/part1_let-const-var/part1.js
+++
@@ -1,7 +0,0 @@
-function userInfo() {
-    const userEmail = 'igor@gmail.com';
-    
-     // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-     console.log(userEmail);
-}
-userInfo();
