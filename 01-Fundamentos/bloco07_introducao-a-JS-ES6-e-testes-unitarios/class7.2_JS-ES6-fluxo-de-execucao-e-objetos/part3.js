2fbd17fc26bb3adae62f175ca22a25120956214e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-14 11:59:06 -0300

    Aquivos do Dia 2 adcicionados.

---
+++ 01-Fundamentos/bloco07_introducao-a-JS-ES6-e-testes-unitarios/class7.2_JS-ES6-fluxo-de-execucao-e-objetos/part3.js
@@ -0,0 +1,51 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  // Exercício 5
+  console.log(allLessons);
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+
+// Exercício 7
+console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
+
+// Exercício 8
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
