bfbdaf6574ac17fee3995470d6718f2618723a54
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-20 14:51:08 -0300

    Arquivos readicionados

---
+++ 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.4_JS-ES6-HOF-reduce/exercises5.js
@@ -0,0 +1,9 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  function containsA() {
+    // escreva seu código aqui
+  }
