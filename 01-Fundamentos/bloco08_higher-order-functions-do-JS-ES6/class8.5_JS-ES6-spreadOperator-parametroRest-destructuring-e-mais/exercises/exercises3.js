2f127cf9536c27d2030bf7b79a8b91a75936e482
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-25 11:47:56 -0300

    Exercício 3 concluído.

--- 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/exercises/exercises3.js
+++ 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/exercises/exercises3.js
@@ -9,15 +9,15 @@ const alex = {
     nationality: 'Australian',
     };
     
-    const gunnar = {
-    name: 'Gunnar',
-    age: 30,
-    likes: ['hiking', 'scuba diving', 'taking pictures'],
-    nationality: 'Icelandic',
-    };
+const gunnar = {
+name: 'Gunnar',
+age: 30,
+likes: ['hiking', 'scuba diving', 'taking pictures'],
+nationality: 'Icelandic',
+};
     
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
 console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
 console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
