665eb4555fc21dface1060037d9975ab74bcadff
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-25 12:12:42 -0300

    Exercício 7 concluído.

--- 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/exercises/exercises7.js
+++ 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/exercises/exercises7.js
@@ -21,6 +21,7 @@ const ships = [
   ];
   
   // escreva shipLength abaixo
+  const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`;
   
   console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
   console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
