934882e627f6274fd51d2e995c4878383742f94d
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-25 11:34:08 -0300

    Exercício de fixação object property shorthand e default parameters concluídos.

--- 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/fixar/objectPropertyShorthand.js
+++ 01-Fundamentos/bloco08_higher-order-functions-do-JS-ES6/class8.5_JS-ES6-spreadOperator-parametroRest-destructuring-e-mais/fixar/objectPropertyShorthand.js
@@ -1,7 +1,9 @@
 // Agora é hora de praticar: altere a função getPosition utilizando a property shorthand.
 
-const getPosition = (latitude, longitude) => ({
-    latitude: latitude,
-    longitude: longitude});
-  
+const getPosition = (latitude, longitude) => {
+  return {
+    latitude,
+    longitude,
+  };
+}
   console.log(getPosition(-19.8157, -43.9542));
