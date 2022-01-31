dbf0cf6ee991e541e3a73d097d4ef993aa7c0559
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-31 10:55:42 -0300

    Exercicios de fixacao de callbacks concluidos.

--- 01-Fundamentos/bloco09_JavaScript-e-testes-assincronos/class9.1_JS-assincrono-e-callbacks/operacoes-assincronas/script.js
+++ 01-Fundamentos/bloco09_JavaScript-e-testes-assincronos/class9.1_JS-assincrono-e-callbacks/operacoes-assincronas/script.js
@@ -1,12 +1,14 @@
-/* setTimeout(() => {
+// ------ Exemplo ------
+setTimeout(() => {
     console.log('Comprar parafusos') // Comprar parafusos
     console.log('Adicionar ao estoque') // Adicionar ao estoque
   }, 2000);
   
   console.log('1 - Receber roda'); // 1 - Receber roda
   console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
-  console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro */
+  console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
 
+// Exercício de fixação 1
 const pushNumber = (list, number) => list.push(number);
 
 let numbers = [];
@@ -17,6 +19,7 @@ pushNumber(numbers, 3);
 
 console.log(numbers);
 
+// Exercício de fixação 2
 const pushNumber = (list, number) => list.push(number);
 
 let numbers = [];
@@ -25,6 +28,6 @@ setTimeout(() => pushNumber(numbers, 1), 3000);
 pushNumber(numbers, 2);
 pushNumber(numbers, 3);
 
-console.log(numbers); // O retorno é [2, 3]
+// console.log(numbers); // O retorno é [2, 3]
 
 setTimeout(() => console.log(numbers), 3000);
