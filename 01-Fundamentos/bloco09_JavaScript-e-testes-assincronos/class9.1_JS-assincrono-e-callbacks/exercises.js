f6015eec06d02f45b6740bc99ed3213738005942
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-31 12:39:36 -0300

    Exercício 4 finalizado.

--- 01-Fundamentos/bloco09_JavaScript-e-testes-assincronos/class9.1_JS-assincrono-e-callbacks/exercises.js
+++ 01-Fundamentos/bloco09_JavaScript-e-testes-assincronos/class9.1_JS-assincrono-e-callbacks/exercises.js
@@ -11,3 +11,20 @@ const getPlanet = () => {
   };
   
   getPlanet(); // imprime Marte depois de 4 segundos
+
+
+// -----> EXERCÍCIO 4 <-----
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+sendMarsTemperature = () => {
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celcius.`), messageDelay());
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
