1100e80ded0da20a1bf868a097c8502286e068ec
Igor Leal <igorhenriqueleal3@gmail.com>
2022-01-31 22:12:25 -0300

    Exercício 1 resolvido.

---
+++ 01-Fundamentos/bloco09_JavaScript-e-testes-assincronos/class9.2_JS-assincrono-fetch-API-async-await/apiScript.js
@@ -0,0 +1,15 @@
+// Resolvido com a ajuda do Rafael Moreci e Guilherme Cunha da Turma 19 - Tribo A
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' }
+  };
+  
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => document.getElementById('jokeContainer').innerText = data.joke);
+}
+
+window.onload = () => fetchJoke();
