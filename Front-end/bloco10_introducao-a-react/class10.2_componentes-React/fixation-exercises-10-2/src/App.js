112bae64b085f5636d5cd8c7af3786e67a311f78
Igor Leal <igorhenriqueleal3@gmail.com>
2022-02-17 18:16:32 -0300

    Modified fixation exercise.

--- Front-end/bloco10_introducao-a-react/class10.2_componentes-React/fixation-exercises-10-2/src/App.js
+++ Front-end/bloco10_introducao-a-react/class10.2_componentes-React/fixation-exercises-10-2/src/App.js
@@ -4,7 +4,7 @@ import Image from './Image';
 function App() {
   return (
     <>
-      <Image source="src/cat.webp" alternativeText="Cute cat staring" />
+      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
     </>
   );
 }
