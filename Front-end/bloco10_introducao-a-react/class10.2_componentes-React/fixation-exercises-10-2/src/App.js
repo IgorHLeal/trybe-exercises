a937052bc09c227b88f93b9654908743f9002219
Igor Leal <igorhenriqueleal3@gmail.com>
2022-02-17 15:12:39 -0300

    Initial files added and fixing exercise 1 completed.

---
+++ Front-end/bloco10_introducao-a-react/class10.2_componentes-React/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+    <>
+      <Image source="src/cat.webp" alternativeText="Cute cat staring" />
+    </>
+  );
+}
+
+export default App;
