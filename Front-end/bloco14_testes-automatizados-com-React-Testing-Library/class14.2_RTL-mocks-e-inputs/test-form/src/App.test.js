48f39842fe6ec43aae77173f93509b3215ab9fed
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-17 22:51:10 -0300

    test: implementação de testes para um formulário em React

--- Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/test-form/src/App.test.js
+++ Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/test-form/src/App.test.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Alterando o valor dos campos e testanso o valor guardado', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const inputName = screen.getByRole('textbox', { name: /nome/i });
+  expect(inputName).toBeInTheDocument();
+  expect(inputName).toHaveValue('');
+
+  // O userEvent simula interações do usuário, ele dispara um evento no elemento da página selecionado.
+  // Ele recebe o type e no primeiro argumento passamos o elemento que queremos interagir, e no segundo a ação de interação, que nesse caso é digitar;
+  userEvent.type(inputName, 'Estudante da Trybe');
+  expect(inputName).toHaveValue('Estudante da Trybe');
+
+  const inputEmail = screen.getByRole('textbox', { name: /email/i });
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveValue('');
+  userEvent.type(inputEmail, 'estudante@trybe.com');
+  expect(inputEmail).toHaveValue('estudante@trybe.com');
+})
