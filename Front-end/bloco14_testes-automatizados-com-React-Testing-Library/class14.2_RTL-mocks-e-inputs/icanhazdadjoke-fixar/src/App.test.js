c1665b198e509c4ab3859da9ed451898ef7964d6
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-17 21:59:28 -0300

    test: implementação do teste para a requisição da API

--- Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/icanhazdadjoke-fixar/src/App.test.js
+++ Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/icanhazdadjoke-fixar/src/App.test.js
@@ -1,8 +1,40 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// O afterEach az com que, após cada teste, o mock seja limpo
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  // A const joke cria um objeto similar ao que é retornado da API 
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  // jest.spyOn espiona as chamadas a função fetch do objeto global
+  // Através do global acessamos qualquer função do sistema
+  // O mock é chamado quando a função fetch é chamada
+  jest.spyOn(global, 'fetch');
+  // Para cada .then na função original, temos um .mockResolvedValue para simular o retorno do fetch
+  // Primeiro retornamos um objeto que contem a função .json e dentro dela criamos um mock que retorna a nossa piada, satisfazendo o que é esperado no nosso componente;
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // O async é usado para esperar até que seja encontrado o que é passado para o await.screen...
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+
+  expect(renderedJoke).toBeInTheDocument();
+  // toBeCalledTimes: garante o número de chamadas ao fetch
+  expect(global.fetch).toBeCalledTimes(1);
+  // toBeCalledWith: garante que o fecth foi chamado com os argumentos corretos
+  expect(global.fetch).toBeCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
 });
+
+// ---------- Outra forma para realizar o teste ----------
