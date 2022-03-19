f0cff8284a15be07110e8e83ea7e00cce3b8a1bb
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-19 10:57:02 -0300

    test: Implementação da primeira parte do exercício 3.

--- Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/exercise-one-of-the-day/service.test.js
+++ Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/exercise-one-of-the-day/service.test.js
@@ -8,11 +8,21 @@ test("testando se a função foi chamada, qual seu retorno e quantas vezes foi c
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
 
-test("testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
+it("testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
   service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
 
   expect(service.randomNumber(10, 2)).toBe(5);
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
   expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
+});
+
+it('Mockando para a função receber 3 parâmetros e retornar sua multiplicalção', () => {
+  // Usar mesma lógica so exercício anterior
+  service.randomNumber = jest.fn().mockImplementation((a,b ,c) => a * b * c);
+
+  expect(service.randomNumber(2, 5, 10)).toBe(100);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(2, 5, 10);
 });
