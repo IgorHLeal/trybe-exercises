70bbe27c1509cf29f275e9d7d6f7e7d08955410e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-19 10:44:09 -0300

    feat: Exercícios 1 e 2 finalizados.

---
+++ Front-end/bloco14_testes-automatizados-com-React-Testing-Library/class14.2_RTL-mocks-e-inputs/exercise-one-of-the-day/service.test.js
@@ -0,0 +1,18 @@
+const service = require('./service');
+
+test("testando se a função foi chamada, qual seu retorno e quantas vezes foi chamada", () => {
+  service.randomNumber = jest.fn().mockReturnValue(10);
+
+  expect(service.randomNumber()).toBe(10);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+});
+
+test("testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros", () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  expect(service.randomNumber(10, 2)).toBe(5);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
+});
