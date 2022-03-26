52b5c575c7eb133b4f7b2f1f0e9de1cc72c3d68e
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-25 22:10:51 -0300

    Exercício 1 finalizado

--- Front-end/bloco15_gerenciamento-de-estado-com-Redux/class15.1_introducao-ao-Redux/color-container-redux/script.js
+++ Front-end/bloco15_gerenciamento-de-estado-com-Redux/class15.1_introducao-ao-Redux/color-container-redux/script.js
@@ -1,7 +1,17 @@
 const ESTADO_INICIAL = {
-  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow', 'gray'],
   index: 0,
 };
+
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
 
 // ---------- ACTIONS ----------
 const actionNextColor = () => {
@@ -10,6 +20,10 @@ const actionNextColor = () => {
 
 const actionPreviousColor = () => {
   store.dispatch({ type: 'PREVIOUS_COLOR' })
+}
+
+const randomColor = () => {
+  store.dispatch({ type: 'RANDOM_COLOR'})
 }
 
 // Crie um reducer, implementando um switch retornando apenas o estado inicial como default
@@ -25,6 +39,12 @@ const reducer = (state = ESTADO_INICIAL, action) => {
         ...state,
         index: state.index === 0 ? state.colors.length -1 : state.index -1,
       }
+    case 'RANDOM_COLOR':
+      return {
+        ...state,
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length,
+      }
     default:
       return state;
   };
@@ -34,8 +54,16 @@ const reducer = (state = ESTADO_INICIAL, action) => {
 // Criar a store
 const store = Redux.createStore(reducer);
 
+store.subscribe(() => {
+const { colors, index } = store.getState();
+document.getElementById('value').innerHTML = colors[index];
+document.getElementById('container').style.backgroundColor = colors[index];
+});
+
 const buttonNext = document.getElementById('next');
 const buttonPrevious = document.getElementById('previous');
+const randomColorId = document.getElementById('random');
 
 buttonNext.addEventListener('click', actionNextColor);
 buttonPrevious.addEventListener('click', actionPreviousColor);
+randomColorId.addEventListener('click', randomColor);
