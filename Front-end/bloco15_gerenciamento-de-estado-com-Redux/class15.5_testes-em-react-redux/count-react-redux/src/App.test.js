5db77b0fc44d4b526c500a1d1a130df7ad6cec9f
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-30 20:45:14 -0300

    exercício de fixação iniciado

--- Front-end/bloco15_gerenciamento-de-estado-com-Redux/class15.5_testes-em-react-redux/count-react-redux/src/App.test.js
+++ Front-end/bloco15_gerenciamento-de-estado-com-Redux/class15.5_testes-em-react-redux/count-react-redux/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Provider } from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
@@ -14,3 +14,22 @@ const renderWithRedux = (
     store,
   }
 }
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+    const count = screen.queryByText('0');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(count).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    const clicksButton = screen.queryByText('5');
+    expect(clicksButton).toBeInTheDocument();
+  });
+});
