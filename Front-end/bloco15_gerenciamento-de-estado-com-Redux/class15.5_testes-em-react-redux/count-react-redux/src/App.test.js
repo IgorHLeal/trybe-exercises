a4d488dfd9658fe44ae2119d1f3e514e164b3316
Igor Leal <igorhenriqueleal3@gmail.com>
2022-03-30 20:04:06 -0300

    arquivos iniciais do exercício de fixação adicionados.

---
+++ Front-end/bloco15_gerenciamento-de-estado-com-Redux/class15.5_testes-em-react-redux/count-react-redux/src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react';
+import { render, cleanup } from '@testing-library/react';
+import App from './App';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
