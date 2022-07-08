1db56c8d2f15b5b2d769ce4da6ade9911ff625b1
Igor Leal <igorhenriqueleal3@gmail.com>
2022-07-08 11:33:04 -0300

    feat: Exercicios do dia 24.1 finalizados.

--- Back-end/bloco24_NodeJs-ORM-e-autenticacao/class24.1_ORM-interface-da-aplicacao-com-o-DB/exercises/models/Book.js
+++ Back-end/bloco24_NodeJs-ORM-e-autenticacao/class24.1_ORM-interface-da-aplicacao-com-o-DB/exercises/models/Book.js
@@ -1,7 +1,9 @@
 const Book = (sequelize, DataTypes) => {
   const Book = sequelize.define("Book", {
     title: DataTypes.STRING,
+    author: DataTypes.STRING,
     page_quantity: DataTypes.INTEGER,
+    publisher: DataTypes.STRING,
     created_at: DataTypes.DATE,
     updated_at: DataTypes.DATE,
   }, {
